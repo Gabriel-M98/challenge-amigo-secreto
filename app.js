6aeebc70d74797640a1b16e9bdde5a2cb7bcb45c
Gabriel Muñoz <gabrielalexis1998@gmail.com>
2025-02-15 17:37:30 -0300

    Primeras líneas de código

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 // Variables
-let nombresUsuario
-// Verificar ingreso de texto válido
-// Alert en caso de no ingreso de texto o ingreso de números
-prompt(alert('Ingreso no válido'));
-// Funciones (creo) para caja de texto, botón agregarAmigo y botón sortearAmigo
+let nombresUsuarios = []
+
+//1. Capturar el nombre escrito por el usuario en el campo de texto.
+document.querySelector('#amigo');
+
+//2. Si el campo está vacío, mostrar alerta y no hacer nada más.
+function agregarAmigo() {
+
+}
+if () {
+
+} else {
+
+}
+
+//3. Si el nombre es válido, agregarlo a un array.
+function actualizarLista() {
+    
+}
+//4. Mostrar la lista de nombres en pantalla.
+document.querySelector('#listaAmigos');
+
+//5. Si el usuario presiona "Sortear", elegir un nombre al azar del array.
+function sortearAmigo() {
+
+}
+
+//6. Mostrar el nombre sorteado en la página.
+//7. Manejar errores (evitar nombres vacíos, evitar sortear si no hay nombres, etc.).
